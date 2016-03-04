09883039f25ff7c5586c68c0880b3c81f656379f
Houssem Dellai <houssem.dellai@live.com>
2016-03-04 17:21:13 +0100

    Added lock.

---
+++ UsingLock/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingLock
+{
+    class Program
+    {
+
+        private static object obj = new object();
+
+        static void Main(string[] args)
+        {
+
+            lock (obj)
+            {
+                
+            }
+        }
+    }
+}
